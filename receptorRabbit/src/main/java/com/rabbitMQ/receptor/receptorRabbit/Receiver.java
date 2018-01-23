2f85b2c340e1f9c7ce6fef5b7c945910609aebdf
Enrique Diaz Ramirez <ediaz@atsistemas.com>
2018-01-23 17:22:36 +0100

    Cambios en gestión de colas

--- receptorRabbit/src/main/java/com/rabbitMQ/receptor/receptorRabbit/Receiver.java
+++ receptorRabbit/src/main/java/com/rabbitMQ/receptor/receptorRabbit/Receiver.java
@@ -2,12 +2,24 @@ package com.rabbitMQ.receptor.receptorRabbit;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 
 public class Receiver {
 
 	public static final String RECEIVE_METHOD_NAME = "receiveMessage";
 	 
+	@Autowired
+    private EurekaClient eurekaClient;
+	
+	@Value("${service.gateway.serviceId}")
+    private String gatewaySearchServiceId;
+	
     public void receiveMessage(String message) {
         System.out.println("[Receiver] ha recibido el mensaje en proyecto RECEPTOR \"" + message + '"');
         //ahora debemos almacenarlo en BBDD. Pero para ello (Al recibir un libro), en vez de mapear
@@ -23,6 +35,8 @@ public class Receiver {
 			e.printStackTrace();
 		}        
         
-        restTemplate.postForObject("http://localhost:8080/book-service/books/save", book,Book.class);		
+        Application application = eurekaClient.getApplication(gatewaySearchServiceId);
+        InstanceInfo instanceInfo = application.getInstances().get(0);
+        restTemplate.postForObject("http://"+instanceInfo.getHostName()+":"+instanceInfo.getPort()+"/book-service/books/save", book,Book.class);		
     }
 }
